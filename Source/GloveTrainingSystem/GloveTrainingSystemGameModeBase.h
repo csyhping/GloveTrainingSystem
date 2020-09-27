bda109a26831e7abddb14c80732485f61d00d2b5
tanshouting <1193250857@qq.com>
2020-09-27 09:41:47 +0800

    initial Commit

---
+++ Source/GloveTrainingSystem/GloveTrainingSystemGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GloveTrainingSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOVETRAININGSYSTEM_API AGloveTrainingSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
